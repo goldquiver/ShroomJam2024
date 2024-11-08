a5ea3fa6a600422d0f9db8feaf642468726be820
Goldquiver <link405@gmail.com>
2024-11-08 15:20:01 -0700

    Add camera to hub map

--- map_hub.gd
+++ map_hub.gd
@@ -1,8 +1,20 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var camera = $Camera2D
+
+@export_range (0, 1) var camera_smoothing: float = 0.06
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play("intro")
+
+
+func _process(_delta):
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		var target_position_x = player.position.x
+		camera.position.x += (target_position_x - camera.position.x) * camera_smoothing
+		var target_position_y = player.position.y
+		camera.position.y += (target_position_y - camera.position.y) * camera_smoothing
