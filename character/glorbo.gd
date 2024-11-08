991f62a6d4b2d804b81b1ec7b89a9046ca2afa69
Goldquiver <link405@gmail.com>
2024-11-08 11:48:05 -0700

    Add a Wander component and make Glorbo Wander

--- character/glorbo.gd
+++ character/glorbo.gd
@@ -1,42 +1,15 @@
 extends CharacterBody2D
 
-@onready var velocity_component = $VelocityComponent
+@onready var wander_component = $WanderComponent
 @onready var anim_sprite = $Visuals/AnimatedSprite2D
-
-var target_position
-
-
-func _ready():
-	target_position = global_position
 
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		target_position.y = target_position.y - 100
-
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
-	
 	set_animation_state(_delta)
-	
-	velocity_component.accelerate_in_direction(direction)
-	velocity_component.move(self)
-
-
-func get_movement_vector():
-	if (global_position.distance_to(target_position) > 5):
-		print(global_position)
-		print(target_position)
-		return Vector2(\
-			target_position.x - global_position.x,\
-			target_position.y - global_position.y)
-	else:
-		target_position = global_position
-		return Vector2.ZERO
 
 
 func set_animation_state(_delta: float):
-	var movement_vector = get_movement_vector()
+	var movement_vector = wander_component.get_movement_vector()
 	
 	if movement_vector.x != 0:
 		if movement_vector.x > 0:
---
+++ component/wander_component.gd
@@ -0,0 +1,46 @@
+extends Node
+
+@export var velocity_component: Node
+@export var active: bool = true
+@export var wander_radius: int = 200
+@export var min_rand_time = 1
+@export var max_rand_time = 8
+
+@onready var timer = $Timer
+
+var ROOT_POSITION: Vector2
+var target_position: Vector2
+
+
+func _ready():
+	ROOT_POSITION = get_parent().global_position
+	target_position = ROOT_POSITION
+	timer.timeout.connect(_on_timer_timeout)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if active:
+		var movement_vector = get_movement_vector()
+		var direction = movement_vector.normalized()
+		velocity_component.accelerate_in_direction(direction)
+		velocity_component.move(velocity_component.get_parent())
+
+
+func get_movement_vector():
+	var parent_pos = get_parent().global_position
+	if (parent_pos.distance_to(target_position) > 5):
+		return Vector2(\
+			target_position.x - parent_pos.x,\
+			target_position.y - parent_pos.y)
+	else:
+		return Vector2.ZERO
+
+
+func _on_timer_timeout():
+	target_position = Vector2(\
+		ROOT_POSITION.x + randf_range(-1, 1)*wander_radius,\
+		ROOT_POSITION.y + randf_range(-1, 1)*wander_radius)
+	
+	timer.wait_time = randf_range(min_rand_time, max_rand_time)
+	timer.start()
