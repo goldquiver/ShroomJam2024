95bd3e165d2ae9b34862a422f9713b1122d02abe
Goldquiver <link405@gmail.com>
2024-11-07 19:40:22 -0700

    Initial Commit

---
+++ character/shroomy.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@onready var velocity_component = $VelocityComponent
+@onready var anim_sprite = $Visuals/AnimatedSprite2D
+
+
+func _process(_delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	
+	set_animation_state(_delta)
+	
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
+
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var y_movement = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return Vector2(x_movement, y_movement)
+
+
+func set_animation_state(_delta: float):
+	var movement_vector = get_movement_vector()
+	
+	if movement_vector.x != 0:
+		if movement_vector.x > 0:
+			anim_sprite.play("walk-right")
+		else:
+			anim_sprite.play("walk-left")
+	else:
+		if movement_vector.y > 0:
+			anim_sprite.play("walk-down")
+		elif movement_vector.y < 0:
+			anim_sprite.play("walk-up")
+		else:
+			anim_sprite.set_frame_and_progress(0, 0)
+			anim_sprite.pause()
