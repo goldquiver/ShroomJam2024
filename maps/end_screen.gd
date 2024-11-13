6e9718fffff838c44657ca770771acf870904627
Goldquiver <link405@gmail.com>
2024-11-12 19:23:39 -0700

    Animate ending screen

--- maps/end_screen.gd
+++ maps/end_screen.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 @onready var splash = $Splash
-@onready var glitch = $Glitch
+@onready var timer = $Timer
 
+var random_arr = [
+	"base", "base", "base", "base", 
+	"blink-glorbo", 
+	"blink-shroomy", 
+	"glitch-1", "glitch-1", "glitch-1", "glitch-1", 
+	"glitch-2", "glitch-2", "glitch-2", "glitch-2", 
+	"glitch-3", "glitch-3", "glitch-3", "glitch-3"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +23,9 @@ func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		var root = State.get_node_in_group("root")
 		root.change_map("res://maps/credits_screen.tscn")
+
+
+func _on_timer_timeout():
+	timer.wait_time += 0.02
+	var anim_name = random_arr.pick_random()
+	splash.play(anim_name)
